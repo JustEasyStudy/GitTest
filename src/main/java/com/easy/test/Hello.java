e2400bfb5560ad8adf83b83caf58193c3ef92fbc
Easy <1095417634@qq.com>
2025-05-22 18:01:17 +0800

    添加Student

--- src/main/java/com/easy/test/Hello.java
+++ src/main/java/com/easy/test/Hello.java
@@ -3,5 +3,6 @@ package com.easy.test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello.main");
+        System.out.println("Hello.main");
     }
 }
---
+++ src/main/java/com/easy/test/Student.java
@@ -0,0 +1,7 @@
+package com.easy.test;
+
+public class Student {
+    public static void main(String[] args) {
+
+    }
+}
